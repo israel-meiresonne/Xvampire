a45546f01b416159f28c0009f518962f7227fb5f
israelmeiresonne <israelmeiresonne97@gmail.com>
2019-02-28 03:04:41 +0100

    Changement de nom des pages secondaire et début écriture header

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'statique#accueil'
 
-  get 'statique/about'
+  get '/about', to: "statique#about"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
